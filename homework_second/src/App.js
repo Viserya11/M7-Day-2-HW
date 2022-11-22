de7336f61052395976e7a5c2fb743e78f529322d
Panna Szűcs <szucspann@gmail.com>
2022-11-22 15:13:52 +0100

    basics

---
+++ homework_second/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import MyNav from './Components/MyNav'
+
+function App() {
+  return (
+    <div>
+      
+      <MyNav subtitle="Random text" />
+      
+      
+    </div>
+  )
+}
+
+
+export default App;
